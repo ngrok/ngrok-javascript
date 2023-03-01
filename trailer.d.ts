d73a8413a670db76023c4a1ebbb75e883349534a
bobzilladev <bob@ngrok.com>
2023-03-01 17:36:56 -0500

    add typedoc capability

--- trailer.d.ts
+++ trailer.d.ts
@@ -1,7 +1,7 @@
 /** Generate, or convert a given tunnel, into one that can be passed into net.Server.listen(). */
 export function listenable(tunnel?: (NgrokHttpTunnel|NgrokTcpTunnel|NgrokTlsTunnel|NgrokLabeledTunnel)): (NgrokHttpTunnel|NgrokTcpTunnel|NgrokTlsTunnel|NgrokLabeledTunnel)
 /** Start the given net.Server listening to a generated, or passed in, tunnel. */
-export function listen(server: net.Server, tunnel?: (NgrokHttpTunnel|NgrokTcpTunnel|NgrokTlsTunnel|NgrokLabeledTunnel)): (NgrokHttpTunnel|NgrokTcpTunnel|NgrokTlsTunnel|NgrokLabeledTunnel)
+export function listen(server: import("net").Server, tunnel?: (NgrokHttpTunnel|NgrokTcpTunnel|NgrokTlsTunnel|NgrokLabeledTunnel)): (NgrokHttpTunnel|NgrokTcpTunnel|NgrokTlsTunnel|NgrokLabeledTunnel)
 /** 
  * Register a console.log callback for ngrok INFO logging.
  * Optionally set the logging level to one of ERROR, WARN, INFO, DEBUG, or TRACE.
